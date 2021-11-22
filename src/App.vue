f1399665277e48357f07ab7512abe6d2c1de7716
Damaris-PC <damisoliveira@gmail.com>
2021-11-22 16:26:31 -0300

    final

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,85 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Escolha uma cor</h1>
+      <input type="color" @change="paletaDeCor($event)">
+  </div>
+  <ExercicioUm>
+    <template v-slot:header="slotA">
+      <h1>{{slotA.title}}</h1>
+    </template>
+
+    <template v-slot:main="slotA">
+      <div>{{slotA.nome}} {{slotA.sobrenome}}</div>
+    </template>
+
+    <template v-slot:footer="slotA">
+       <div>{{slotA.nome}} {{slotA.sobrenome}}</div>
+    </template>
+  </ExercicioUm>
+
+  <div id="app">
+    <h2>Barra de Cores</h2>
+    <Canvas style="width: 100%; height: 600px;">
+      <Caixa
+        v-for="(obj, index) of chartValues"
+        :key=index
+        :x1="(index / chartValues.length) * 100"
+        :x2="(index / chartValues.length) * 100 + 100 / chartValues.length"
+        :y1="100"
+        :y2="100 - obj.val"
+        :color="obj.color"
+        :value="obj.val"
+      >
+      </Caixa>
+    </Canvas>
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ExercicioUm from './components/ExercicioUm.vue'
+import Canvas from './components/Canvas.vue'
+import Caixa from './components/Caixa.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    ExercicioUm,
+    Caixa,
+    Canvas
+  },
+   data() {
+    return {
+      chartValues: [
+        { val: 24, color: 'red' },
+        { val: 32, color: '#0f0' },
+        { val: 66, color: 'rebeccapurple' },
+        { val: 1, color: 'green' },
+        { val: 28, color: 'blue' },
+        { val: 60, color: 'rgba(150, 100, 0, 0.2)' },
+      ],
+    };
+  },
+  methods: {
+  paletaDeCor(e){
+    console.log(e.target.value)
+    }
+  },
+  mounted() {
+    let dir = 1;
+    let selectedVal = Math.floor(Math.random() * this.chartValues.length);
+
+    setInterval(() => {
+      if (Math.random() > 0.995) dir *= -1;
+      if (Math.random() > 0.99)
+        selectedVal = Math.floor(Math.random() * this.chartValues.length);
+
+      this.chartValues[selectedVal].val = Math.min(
+        Math.max(this.chartValues[selectedVal].val + dir * 0.5, 0),
+        100
+      );
+    }, 16);
+  },
 }
 </script>
 
---
+++ src/components/ExercicioUm.vue
@@ -0,0 +1,74 @@
+<template>
+<div class="container">
+  <header v-bind:style="styleObject">
+    <slot name="header" :title='title'></slot>
+  </header>
+  <main v-for="x in names" :key="x.nome" v-bind:style="x.styleObject">
+      <div>
+        <slot name="main" :nome="x.nome" :sobrenome="x.sobrenome"></slot>
+      </div>
+  </main>
+  <footer  v-for="x in names" :key="x.nome"  >
+    <slot name="footer" :nome="x.nome" :sobrenome="x.sobrenome"  ></slot>
+  </footer>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'ExercicioUm',
+  data(){
+      return {
+        title: 'Lista de nomes',
+        styleObject: {
+            color: 'red',
+            fontSize: '13px'
+        },
+        names : [
+              {nome: 'Monteiro', sobrenome: 'Lobato',  
+               styleObject: {
+                    color: 'blue',
+                    fontSize: '30px'
+              }},
+              {nome: 'José' , sobrenome: 'de Alencar',  
+                styleObject: {
+                    color: 'blue',
+                    fontSize: '30px'
+              }},
+              {nome: 'Cecília', sobrenome: 'Meireles',  
+                styleObject: {
+                    color: 'red',
+                    fontSize: '20px'
+              }},
+              {nome: 'Clarice', sobrenome: 'Lispector',  
+                styleObject: {
+                    color: 'red',
+                    fontSize: '20px'
+              }},
+              {nome: 'Mario', sobrenome: 'Quintana',  
+                styleObject: {
+                    color: 'blue',
+                    fontSize: '30px'
+              }}
+        ]
+    }
+  },
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
